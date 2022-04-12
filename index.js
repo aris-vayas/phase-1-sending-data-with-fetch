ac0ea26dd2b0f5ef0e91f572e04a44b340cddb63
Aris Vayas <aris.vayas@gmail.com>
2022-04-12 16:52:45 -0400

    commit tree 8da63d44599b8f7a5b3974a0eafec71f2d35abb8

--- index.js
+++ index.js
@@ -1,1 +1,35 @@
-// Add your code here
+//create a function to subit json data
+ 
+// make an object with key:value as name and email
+const formData = {
+  name: "Steve",
+  email: "steve@steve.com",
+};
+
+//POST form JSON
+const configurationObject = {
+  method:"POST",
+  headers: {
+    "Content-Type": "application/json",
+    Accept: "application/json",
+  },
+  body: JSON.stringify(formData),
+};
+
+//will fecth and sumbit our above objects
+//returns our fetch() bases on above arguements
+function submitData(){
+    return fetch("http://localhost:3000/users", configurationObject)
+    .then(function (response) {
+      return response.json();
+    })
+    .then(function (object) {
+      let p=document.createElement('p')
+
+      document.body.append(p)
+     return p.innerText=object.id
+      //return console.log(p);
+    })}
+
+
+submitData()
